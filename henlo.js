84811e3e2f09f29c95e341e9cc1f76d40f65d6dd
Barnaby Collins <barnabyc@sky.com>
2019-02-14 12:38:05 +0000

    add tater searching

--- henlo.js
+++ henlo.js
@@ -1,6 +1,7 @@
 console.log('starting henlo');
 var express = require('express');
 var app = express();
+var taters = require('./taters.json');
 
 app.get('/', function(req, resp) {
     resp.send('hello world');
@@ -15,6 +16,17 @@ app.get('/slap', function(req, resp) {
     name = req.query.name;
     item = req.query.item;
     resp.send('*slaps ' + name + ' with ' + item + '*');
+})
+
+app.get('/taters', function(req, resp) {
+    search = req.query.search;
+    response = '';
+    for (i = 0; i < taters.length; i++) {
+        if (taters[i].title.toLowerCase().indexOf(search) !== -1) {
+            response += '<img height="100" src="' + taters[i].thumbnail + '"><br><a href="' + taters[i].href + '">' + taters[i].title + '</a><br><br>';
+        }
+    }
+    resp.send(response);
 })
 
 app.get('*', function(req, resp) {
