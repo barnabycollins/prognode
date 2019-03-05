18eefd5e5265467f01ee2e3790ecd5e51b3c68b4
Barnaby Collins <barnabyc@sky.com>
2019-03-05 15:36:52 +0000

    Add ability to add or update user

--- project/server.js
+++ project/server.js
@@ -1,15 +1,32 @@
-var bookings = [];
-var users = [];
-
 class User {
 	/**
 	* Class for a user
 	* @param {??} id user's id
+	* @param {string} name user's name
 	* @param {number} permissionLevel the permission level of the user
 	*/
-	constructor (id, permissionLevel) {
+	constructor (id, name, permissionLevel) {
 		this.id = id;
+		this.name = name
 		this.permissionLevel = permissionLevel;
+	}
+}
+
+/**
+* Add or update user entry
+* @param {number} id ID of the user to add or update
+* @param {string} name name to give to the user
+*/
+function updateUser(id, name) {
+	var haveUser = false;
+	for (i = 0; i < users.length; i++) {
+		if (users[i].id == id) {
+			haveUser = true;
+			users[i].name = name;
+		}
+	}
+	if (!haveUser) {
+		users.push(new User(id, name, 0));
 	}
 }
 
@@ -48,13 +65,18 @@ function createBooking(STime, ETime, name, id, recurrence) {
 	for (i = 0; i < users.length; i++) {
 		if (users[i].id == id) {
 			haveUser = true;
+			name = users[i].name;
 		}
 	}
 	if (!haveUser) {
-		users.push(new User(id, 0));
+		users.push(new User(id, name, 0));
 	}
 	bookings.push(new Booking(Date.now(), STime, ETime, name, id, recurrencedict[recurrence]));
 }
+
+
+var users = [new User(1337, 'Barnaby Collins', 9)];
+var bookings = [];
 
 
 // NODE SERVER
@@ -75,8 +97,15 @@ app.get('/bookings', function(req, resp) {
 	resp.send(bookings);
 });
 
+/* GETTING USER LIST */
 app.get('/users', function(req, resp) {
 	resp.send(users);
+});
+
+/* ADD OR UPDATE USER ENTRY */
+app.post('/updateuser', function(req, resp) {
+	updateUser(req.body.id, req.body.name);
+	resp.send('User successfully updated');
 });
 
 /* NEW BOOKING */
