99fc72f0e55f784bd0aaa7e8d6484e65e02197a3
Barnaby Collins <barnabyc@sky.com>
2019-05-09 17:00:11 +0100

    Fix a couple issues with deployment

--- project/assets/testbk.js
+++ project/assets/testbk.js
@@ -1,8 +1,6 @@
-const bs = require('../booksys');
+//const bs = require('../booksys');
 
 function addTestBookings() {
-	bs.createBooking('03/05/2019', '10:00', '12:00', 'steve', {'sub': '80', 'email': 'steve@stevecorp.org', 'name': 'STEPHEN'}, false);
-	bs.createBooking('04/05/2019', '16:00', '19:00', '', {'sub': '116714588086254124711', 'email': 'barnstormer322@gmail.com', 'name': 'Barnaby Collins'}, false);
-	bs.createBooking('02/05/2019', '20:00', '22:00', 'Recurring booking', {'sub': '116714588086254124711', 'email': 'barnstormer322@gmail.com', 'name': 'Barnaby Collins'}, true);
+	return true;
 }
 module.exports = {addTestBookings};
